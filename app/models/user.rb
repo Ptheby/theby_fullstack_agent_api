94ab90dee47d6aa384d7aa4bf8902a6394de4edc
Paul Theby <paultheby@gmail.com>
2024-02-24 10:39:22 -0600

    erase _has_secure_password in in usermodel

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
     validates :role, presence:true,inclusion: { in: %w(admin user), message: "%{value} is not a valid role" }
     validates :password_digest, presence:true
  
-
+   
     has_one :agent
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
-Rails.application.routes.draw do
-resources :users,:agents
-end
+resources :users do
+    post 'create_with_agent', on: :collection
+  end
+
+  # "resources :users do" generates RESTful routes for the User model. It provies standard CRUD routes for working with the user resource.
+#'post 'create_with_agent', on: :collection' this line adds a customer action named 'create_with_agent' to the user resource. the on: :collection option means that this 
+#action operates on the entire collection of users, not a specific user. W
+#with these routes you have a standard RESTful routes for managing users and youve added a custom route to handle the specific case of creating a user with an associated agent. 
