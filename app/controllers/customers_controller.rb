4510b9ed6ea5a9f3afeaf3108a06e3dd7e896799
Paul Theby <paultheby@gmail.com>
2024-03-05 22:57:50 -0600

    troubleshooting customer creation
    ongoing problem with foreign key constraints. Troubleshooting for hours and asking for help tomorrow

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,54 +1,32 @@
 class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :update, :destroy]
-  # before_action :authenticate_request     activate this later!
 
   def index
     @customers = Customer.all
-    render json: @customers
+    render json: CustomerBlueprint.render(@customers, view: :default)
   end
 
   def show
-    render json: { customer: @customer }
+    render json: CustomerBlueprint.render(@customer, view: :default)
   end
 
-  # def create
-  #   # Extract address_attributes without customer_id
-  
-
-  #   @customer = Customer.new(customer_params)
-  #   address_attributes = params[:customer].delete(:address_attributes)
-
-  #   # Build the address using the modified address_attributes
-  #   new_address = @customer.build_address(address_attributes)
-
-  #   if @customer.valid?
-  #     @customer.save
-  #     render json: { customer: @customer }, status: :created
-  #   else
-  #     render json: { customer_errors: @customer.errors }, status: :unprocessable_entity
-  #   end
-  # end
-
   def create
-    # Permit all customer attributes, including address_attributes
     @customer = Customer.new(customer_params)
   
-    if @customer.save
-      render json: { customer: @customer }, status: :created
-    else
-      render json: { customer_errors: @customer.errors }, status: :unprocessable_entity
+    begin
+      if @customer.save
+        render json: CustomerBlueprint.render(@customer, view: :default), status: :created
+      else
+        render json: { customer_errors: @customer.errors }, status: :unprocessable_entity
+      end
+    rescue ActiveRecord::InvalidForeignKey => e
+      render json: { error: e.message }, status: :unprocessable_entity
     end
   end
 
-
-
   def update
-    updated_address_attributes = params[:customer][:address_attributes]
-    existing_address = @customer.addresses.first_or_initialize
-    existing_address.update(updated_address_attributes)
-
     if @customer.update(customer_params)
-      render json: { customer: @customer }
+      render json: CustomerBlueprint.render(@customer, view: :default)
     else
       render json: { customer_errors: @customer.errors }, status: :unprocessable_entity
     end
@@ -74,9 +52,7 @@ class CustomersController < ApplicationController
       :email,
       :agent_id,
       :insurance_company_id,
-      address_attributes: [:street_number, :street_name, :city, :state, :zip],
-
-      
+      address_attributes: [:street_number, :street_name, :city, :state, :zip]
     )
   end
 end
