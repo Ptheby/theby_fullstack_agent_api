87635bee50d6e7fa078f3a548d17306652238ba7
Paul Theby <paultheby@gmail.com>
2024-03-07 20:00:20 -0600

    ADD(validation)to agent model

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -3,6 +3,7 @@ class Agent < ApplicationRecord
   validates :first_name, presence:true
   validates :last_name, presence:true
   validates :npn, presence:true, numericality:true
+  validates :state, presence:true
  
   belongs_to :user
  
