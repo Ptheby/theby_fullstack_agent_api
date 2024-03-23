cb9d3d6b129c598e4309a291d01927b5b115879e
Paul Theby <paultheby@gmail.com>
2024-03-23 12:35:01 -0500

    Update agent_blueprint.rb
    remove city

--- app/blueprints/agent_blueprint.rb
+++ app/blueprints/agent_blueprint.rb
@@ -1,6 +1,6 @@
 class AgentBlueprint < Blueprinter::Base
   identifier :id
-  fields :first_name, :last_name, :npn, :state, :city
+  fields :first_name, :last_name, :npn, :state
 
   association :user, blueprint: UserBlueprint, view: :default
   
