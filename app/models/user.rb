ba31dfc4e247c2c1a7b02e54e3105da88077e969
Paul Theby <paultheby@gmail.com>
2024-03-22 20:22:40 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :email, presence:true, email:true, uniqueness: true
+    validates :email, presence:true, uniqueness: true
     
     # def attributes
     #     super.merge('email' => email)
