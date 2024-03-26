9708413b84affd20f5c099d02c4242efe5479237
Paul Theby <paultheby@gmail.com>
2024-03-25 19:14:30 -0500

    login routes
    corrected routes for login to be independent of user resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   resources :users, only: [:create, :index, :show, :update, :destroy] do
-    post '/login', to: 'sessions#create'
     collection do
       post 'create_with_agent'
     end
   end
+
+  post '/login', to: 'sessions#create'  # Define login route here
 
   resources :customers do
     resources :addresses, only: [:create, :update, :destroy]
   end
 
   resources :agents, only: [:index, :update]
-
-
 end
