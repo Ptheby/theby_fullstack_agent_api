75c6070417b0dd40a3078d0d8bd897121d0b84af
Paul Theby <paultheby@gmail.com>
2024-03-27 17:40:37 -0500

    Update address.rb
    checking

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ApplicationRecord
     belongs_to :customer
-    validates :customer_id, presence: true
+    # validates :customer_id, presence: true
     validates :street_number, :street_name, :city, :state, :zip, presence: true
   end
   
