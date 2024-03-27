c7a1a2feaf2f2c62579d2737290a2c57a6e0e3d9
Paul Theby <paultheby@gmail.com>
2024-03-27 16:32:34 -0500

    add customer Id to address migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_24_035719) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_27_213057) do
   create_table "addresses", force: :cascade do |t|
     t.integer "street_number"
     t.string "street_name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_24_035719) do
     t.integer "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "customer_id", null: false
+    t.index ["customer_id"], name: "index_addresses_on_customer_id"
   end
 
   create_table "agents", force: :cascade do |t|
@@ -78,6 +80,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_24_035719) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "addresses", "customers"
   add_foreign_key "agents", "users"
   add_foreign_key "customers", "addresses"
   add_foreign_key "customers", "agents"
