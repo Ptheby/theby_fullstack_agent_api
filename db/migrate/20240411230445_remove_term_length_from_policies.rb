933851e74767e308f021eb0358de6925bc2918cd
Paul Theby <paultheby@gmail.com>
2024-04-16 21:10:17 -0500

    Delete 20240411230445_remove_term_length_from_policies.rb
    fixing migrations on term_length

--- db/migrate/20240411230445_remove_term_length_from_policies.rb
+++
@@ -1,6 +0,0 @@
-class RemoveTermLengthFromPolicies < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :policies, :termlength
-   change_column :policies, :term_length, :string
-  end
-end
