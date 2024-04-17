f8d80393fa53701bff045b340c678ec760c2e819
Paul Theby <paultheby@gmail.com>
2024-04-16 21:07:32 -0500

    Update 20240410201709_change_termlength_type_in_policy.rb
    corrected typo in migration

--- db/migrate/20240410201709_change_termlength_type_in_policy.rb
+++ db/migrate/20240410201709_change_termlength_type_in_policy.rb
@@ -1,5 +1,5 @@
 class ChangeTermlengthTypeInPolicy < ActiveRecord::Migration[6.0]
   def change
-    change_column :policies, :termlength, :string
+    change_column :policies, :term_length, :string
   end
 end
